67713944a0414107174d1cc34ce7c24bf9f3c6c5
Chris Kelley <chriskelley85@gmail.com>
2022-02-22 15:54:16 -0600

    login router

--- src/App.js
+++ src/App.js
@@ -1,38 +1,26 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import Login from './components/Login';
 
-
-const Login = () => {
-  return <h2>Login</h2>
-}
-
-const FriendsList = () => {
-  return <h2>Friends List</h2>
-}
-
-const AddFriend = () => {
-  return <h2>Add Friend</h2>
-}
 function App() {
   return (
-    <Router>
-    <div className="App">
-      <Route path="/">
-        <Login />
-      </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
-      <Route path="/friends">
-        <FriendsList />
-      </Route>
-      <Route path="/friends/add">
-        <AddFriend />
-      </Route>
-    </div>
-  </Router>
+      <div className="App">
+        <header>
+          <nav>
+            <Link to='/login'>Login</Link>
+            <a href='#'>Friends</a>
+            <a href='#'>Add friend</a>
+            <a href='#'>Logout</a>
+          </nav>
+        </header>
+        <Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
+        </Switch>
+      </div>
   );
-}
+};
 
 export default App;
