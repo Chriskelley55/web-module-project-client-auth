bdae4b188bd4ebcd79f8ff60351cd9b133541047
Chris Kelley <chriskelley85@gmail.com>
2022-02-22 15:45:40 -0600

    login

--- src/App.js
+++ src/App.js
@@ -2,13 +2,37 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
-  );
+
+const Login = () => {
+  return <h2>Login</h2>
 }
 
+const FriendsList = () => {
+  return <h2>Friends List</h2>
+}
+
+const AddFriend = () => {
+  return <h2>Add Friend</h2>
+}
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Route path="/">
+        <Login />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/friends">
+        <FriendsList />
+      </Route>
+      <Route path="/friends/add">
+        <AddFriend />
+      </Route>
+    </div>
+  </Router>
+  );
+}
 
 export default App;
